e327248e8843d9beb78f3e15c526e750db1726fb
Puk Jennekens <puk@elasticweb.nl>
2026-01-03 15:03:35 +0100

    Added the minifigs table

---
+++ database/migrations/2026_01_03_140041_create_minifigs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('minifigs', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('rebrickable_id', 20)->unique();
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('minifigs');
+    }
+};
